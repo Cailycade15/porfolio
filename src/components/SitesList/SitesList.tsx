897a255d474d68bab4a1ac4c6e65f15ed1e1a931
Cailycade <pencovigor@gmail.com>
2026-03-23 23:23:10 +0300

    Add homepage and add to scriot in packaje.json

--- src/components/SitesList/SitesList.tsx
+++ src/components/SitesList/SitesList.tsx
@@ -5,7 +5,8 @@ import Card from "../Card/Card"
 import CardRotateEffect from "../CardRotateEffect/CardRotateEffect"
 
 interface SitesList {
-    list: any[]
+    // list: any[]
+    
 }
 
 // {list}: SitesList
@@ -16,8 +17,8 @@ const SitesList = () => {
             <CardRotateEffect count={3} deg={200}>
                 {[
                     <a href="/Sites/CleanSite/main.html"><Card img_path="/img_for_cards/CleanSiteCard.png" main_word_1="Clean" main_word_2="Site"/></a>,
-                    <a href="/Sites/CleanSite/main.html"><Card img_path="/img_for_cards/CleanSiteCard.png" main_word_1="Clean" main_word_2="Site"/></a>,
-                    <a href="/Sites/CleanSite/main.html"><Card img_path="/img_for_cards/CleanSiteCard.png" main_word_1="Clean" main_word_2="Site"/></a>, 
+                    <a href="/Sites/Mogo/index.html"><Card img_path="/img_for_cards/Mogo.png" main_word_1="Mogo" main_word_2="Site"/></a>,
+                    <a href="/Sites/KroppFitness/index.html"><Card img_path="/img_for_cards/KropFit.png" main_word_1="KropFit" main_word_2="Site"/></a>, 
                 ]}
             </CardRotateEffect>
 
